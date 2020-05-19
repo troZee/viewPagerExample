579f9bfe2dc9ae5ca8acf5a79b066ca208952003
Piotr Trocki <piotr.trocki@callstack.com>
2020-05-19 14:05:34 +0200

    fresh project

--- android/app/src/main/java/com/asd/MainActivity.java
+++ android/app/src/main/java/com/asd/MainActivity.java
@@ -1,4 +1,4 @@
-package com.viewpagerexample;
+package com.asd;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "viewPagerExample";
+    return "asd";
   }
 }
